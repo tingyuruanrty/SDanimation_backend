896a70fea8c94a792d03378b81053dadfaa0ea2c
Tingyu Ruan <tingyuruanrty@gmail.com>
2026-09-11 19:46:10 -0700

    have learnt a bit about the sqlalchemy, database.py create the db file if it doesn't exist, and is ready to give out sessions to user

--- database.py
+++ database.py
@@ -4,17 +4,27 @@ from sqlalchemy.orm import declarative_base, sessionmaker, Mapped, mapped_column
 # 1. Database URL: SQLite stores data locally in 'characters.db'
 # When switching to PostgreSQL later, just change this string to:
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@localhost/dbname"
-SQLALCHEMY_DATABASE_URL = "sqlite:///./characters.db"
+# it gives engine three important fact: 1, what database , sqlite in this case; 2, database api, it could be omitted, default option will take in charge; 3, 
 
+# make engine
+SQLALCHEMY_DATABASE_URL = "sqlite:///./characters.db"
+# establish connections to database
 engine = create_engine(
+    # URL string for the database
     SQLALCHEMY_DATABASE_URL,
     # SQLite requires this flag when used with multiple threads in FastAPI
     connect_args={"check_same_thread": False}
 )
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+# i want to use the facade of sqlalchemy, which is session
 
+# it's a boilerplate for session
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+# base is the blueprint of table
+# make table
+Base = declarative_base()
 # 2. Define the Character Relation (Table)
 class Character(Base):
     __tablename__ = "characters"
@@ -25,6 +35,8 @@ class Character(Base):
     lora_filename: Mapped[str] = mapped_column(String, nullable=False)
 
 # 3. Create all tables on startup
+# metadata is like the blue print book, the tables are bluepritn
+# create the data base
 Base.metadata.create_all(bind=engine)
 
 # 4. Dependency to yield database sessions per request
